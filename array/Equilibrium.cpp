542875c7529eb33413a2401fabcd72b99417131d
Avinash Tiwari <114090381+Avinash614-eng@users.noreply.github.com>
2023-02-27 23:25:04 +0530

    Update Equilibrium.cpp

--- array/Equilibrium.cpp
+++ array/Equilibrium.cpp
@@ -1,1 +1,40 @@
+ #include<iostream>
+ using namespace std;
+
+ 
+ 
+ 
+ int equilibriumPoint(long long a[], int n) {
+    
+        if(n==1)
+          return 1;
+        if(n==2)
+          return -1;
+      
+        int s1=0;int s2=0;
+          
+        for(int i=1;i<n;i++)
+            {
+                s2=s2+a[i];
+            }
+           
+     for(int i=0;i<n-1;i++)
+     {
+        if(s1==s2)
+            return i+1;
+
+        s1+=a[i];
+        s2-=a[i+1];
+     }
+        return -1;
+}
+//fggkjkj
+
+int main()
+{
+    long long int a[7]={1,7,3,5,2,2,7};
+   long long int n=7;
+
+   int t=equilibriumPoint(a,n);
+    cout<<t<<endl;
 }
